e4d5fce3419645f0b07ff530e38be0c0b41e20d8
MaXa0H <58989558+MaXa0H@users.noreply.github.com>
2026-09-11 22:10:45 +0300

    Create unique IPC session for each app launch

--- src/Rdptun.App/Program.cs
+++ src/Rdptun.App/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Rdptun.App
@@ -8,6 +10,13 @@ namespace Rdptun.App
         [STAThread]
         private static void Main()
         {
+            int pid = Process.GetCurrentProcess().Id;
+            string token = Guid.NewGuid().ToString("N");
+            Environment.SetEnvironmentVariable("RDPTUN_PIPE", "rdptun-control-" + pid + "-" + token);
+            Environment.SetEnvironmentVariable(
+                "RDPTUN_TRACE",
+                Path.Combine(Path.GetTempPath(), "rdptun-plugin-" + pid + "-" + token + ".log"));
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
